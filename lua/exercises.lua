085e61c8c83f15eb96fb18368e50a292fae803e1
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2024-09-04 16:49:47 -0700

    first lua function

--- lua/exercises.lua
+++ lua/exercises.lua
@@ -13,12 +13,39 @@ function change(amount)
   return counts
 end
 
--- Write your first then lower case function here
+function first_then_lower_case(list_of_strings, predicate)
+  -- Check to make sure list isn't empty
+  if list_of_strings == {} then
+    -- if it is, return nil
+    return nil
+  end
+  -- Check each string in the list of strings if it can be looped through 
+  for _, my_string in ipairs(list_of_strings) do
+    -- If it (as a lowercase) satisfies the predicate, return that string
+    if (predicate(string.lower(my_string))) then
+      return string.lower(my_string)
+    end
+  end
+  -- If no strings satisfy the predicate, return nil
+  return nil
+end
 
 -- Write your powers generator here
+function powers_generator()
+
+end
 
 -- Write your say function here
+function say()
+
+end
 
 -- Write your line count function here
+function meaningful_line_count()
+
+end
 
 -- Write your Quaternion table here
+Quaternion = (function(class)
+
+end) ({})
