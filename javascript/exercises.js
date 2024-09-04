7592779bca2e806eff3be673328124db205cf789
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2024-09-04 12:23:03 -0700

    All js tests passing

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -94,5 +94,105 @@ export async function meaningfulLineCount(filePath) {
 
 // Write your Quaternion class here
 export class Quaternion {
+  // setup and store our properties
+  constructor(a, b, c, d) {
+    Object.assign(this, {a, b, c, d})
+    // don't allow people to change the properties directly
+    Object.freeze(this)
+  }
+
+  // add coeficients as a property
+  get coefficients() {
+    return [this.a, this.b, this.c, this.d]
+  }
+
+  // add conjugate as a property
+  get conjugate() {
+    return new Quaternion(this.a, -this.b, -this.c, -this.d)
+  }
+
+  // lets us check if quaternions are equal
+  equals(that) {  // I am very funny
+    return (this.a === that.a && this.b === that.b && this.c === that.c && this.d === that.d)
+  }
+
+  // handles addition
+  plus(that) {
+    return new Quaternion (this.a + that.a, this.b + that.b, this.c + that.c, this.d + that.d)
+  }
+
+  // handles multiplication
+  times(that) {
+    const a = this.a * that.a - this.b * that.b - this.c * that.c - this.d * that.d
+    const b = this.a * that.b + this.b * that.a + this.c * that.d - this.d * that.c
+    const c = this.a * that.c - this.b * that.d + this.c * that.a + this.d * that.b
+    const d = this.a * that.d + this.b * that.c - this.c * that.b + this.d * that.a
+    return new Quaternion (a, b, c, d)
+  }
+
+  // Convert quaternion into a string in the format a+bi+cj+dk
+  // Additional format stuff: Only display non-zero values. Don't print out value of one. Subtract instead of adding negative numbers. If all zeros, return "0"
+  toString() {
+    // Edge case: all zeros
+    if (this.coefficients == [0,0,0,0]) {
+      return "0"
+    } 
+
+    // store the string we will return
+    let quatStr = ""
+    
+    // A - if there is a non-zero value, add it to the string
+    if (this.a !== 0) {
+      quatStr += `${this.a}`;
+    }
   
+    // B - handle the "i" component
+    if (this.b !== 0) {
+      if (quatStr && this.b > 0) {
+        quatStr += "+"; // only the "+" needs to be added explicitly because the "-" is included in negative numbers
+      }
+        if (this.b === 1) {
+          quatStr += "i";
+        } else if (this.b === -1) {
+          quatStr += "-i";
+        } else {
+          quatStr += `${this.b}i`;
+        }
+      }
+  
+    // C - handle the "j" component
+    if (this.c !== 0) {
+      if (quatStr && this.c > 0) {
+        quatStr += "+"; // only the "+" needs to be added explicitly because the "-" is included in negative numbers
+      }
+      if (this.c === 1) {
+        quatStr += "j";
+      } else if (this.c === -1) {
+        quatStr += "-j";
+      } else {
+        quatStr += `${this.c}j`;
+      }
+    }
+  
+    // D - handle the "k" component
+    if (this.d !== 0) {
+      if (quatStr && this.d > 0) {
+        quatStr += "+"; // only the "+" needs to be added explicitly because the "-" is included in negative numbers
+      }
+      if (this.d === 1) {
+        quatStr += "k";
+      } else if (this.d === -1) {
+        quatStr += "-k";
+      } else {
+        quatStr += `${this.d}k`;
+      }
+    }
+    
+    // Edge case: all zeros
+    if (quatStr == "") {
+      return "0"
+    }
+    // Return the completed string
+    return quatStr
+  }
 }
