c36f05daa9007dfe5483bff0139e7bd7ceeb2425
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2024-10-18 16:56:17 -0700

    shape type

--- typescript/exercises.ts
+++ typescript/exercises.ts
@@ -29,7 +29,7 @@ export function firstThenApply<T, U>(
 export function* powersGenerator(base: bigint): Generator<bigint> {
   let exponent: bigint = 0n
 
-  // while loop is needed to return additional values when powersGenerateor is called again
+  // While loop is needed to return additional values when powersGenerateor is called again
   while (true) {
     yield base ** exponent
     exponent += 1n
@@ -39,10 +39,10 @@ export function* powersGenerator(base: bigint): Generator<bigint> {
 export async function meaningfulLineCount(filePath: any) {
   let lineCount: number = 0
 
-  // read each line of the code
+  // Read each line of the code
   const file = await open(filePath, "r")
   for await (const line of file.readLines()) {
-    //  if the line is non-empty and non-leading-#, count it
+    //  If the line is non-empty and non-leading-#, count it
     if (line.trim() != "" && line.trim()[0] != "#") {
       lineCount++
     }
@@ -51,6 +51,47 @@ export async function meaningfulLineCount(filePath: any) {
   return lineCount
 }
 
-// Write your shape type and associated functions here
+// TODO ask about tostring
+export type Shape = Box | Sphere
+
+interface Box {
+  // Properties of a box
+  kind: "Box"
+  width: number
+  length: number
+  depth: number
+}
+
+interface Sphere {
+  // Properties of a sphere
+  kind: "Sphere"
+  radius: number
+}
+
+export function volume(shape: Shape): number {
+  if (shape.kind === "Box") {
+    // Gets the dimensions from the box interface
+    const { width, length, depth } = shape
+    return width * length * depth
+  }
+
+  // Shape only has Boxes and Spheres so no else if statement needed
+  // Gets the radius from the sphere interface
+  const { radius } = shape
+  return (4 / 3) * Math.PI * Math.pow(radius, 3)
+}
+
+export function surfaceArea(shape: Shape): number {
+  if (shape.kind === "Box") {
+    // Gets the dimensions from the box interface
+    const { width, length, depth } = shape
+    return 2 * (width * length + width * depth + length * depth)
+  }
+
+  // Shape only has Boxes and Spheres so no else if statement needed
+  // Gets the radius from the sphere interface
+  const { radius } = shape
+  return 4 * Math.PI * Math.pow(radius, 2)
+}
 
 // Write your binary search tree implementation here
