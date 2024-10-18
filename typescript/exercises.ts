aff1933de6f16fde5b1c084479defba669b1cca9
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2024-10-18 11:12:38 -0700

    adding firstThenApply

--- typescript/exercises.ts
+++ typescript/exercises.ts
@@ -13,7 +13,14 @@ export function change(amount: bigint): Map<bigint, bigint> {
   return counts
 }
 
-// Write your first then apply function here
+// TODO: ask about <T, U> vs any vs String | number
+export function firstThenApply<T, U>(a: Array<T>, p: (s: T) => boolean, f: (x: T) => U): U | undefined {
+  // Use find() to search for the first value from the array that satisfies the predicate
+  const found = a.find((value) => p(value));
+
+  // If a value was found, return the result of applying the function, otherwise return undefined
+  return found !== undefined ? f(found) : undefined;
+}
 
 // Write your powers generator here
 
