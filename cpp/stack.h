d418691f7dc9d49b32dc41a53eef12a41bd8515f
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2024-11-21 13:20:35 -0800

    Finished C++ stack

--- cpp/stack.h
+++ cpp/stack.h
@@ -61,7 +61,7 @@ public:
     {
       reallocate(2 * capacity);
     }
-    elements(top++) = item;
+    elements[top++] = item;
   }
 
   T pop()
@@ -75,9 +75,11 @@ public:
       reallocate(capacity / 2);
     }
     // get top element, pop it
+    T popped = elements[--top];
     // overwrite the top element with the default value for security
+    elements[top] = T{};
     // return the top value
-    return elements[--top];
+    return popped;
   }
 
 private:
@@ -89,5 +91,26 @@ private:
   // array with std::copy().
   void reallocate(int new_capacity)
   {
+    // Ensure new capacity is within bounds
+    if (new_capacity > MAX_CAPACITY)
+    {
+      new_capacity = MAX_CAPACITY;
+    }
+    if (new_capacity < INITIAL_CAPACITY)
+    {
+      new_capacity = INITIAL_CAPACITY;
+    }
+
+    // Create a new array with the new capacity
+    unique_ptr<T[]> new_elements = make_unique<T[]>(new_capacity);
+
+    // Copy the elements from the old array to the new array
+    copy(&elements[0], &elements[top], &new_elements[0]);
+
+    // Transfer ownership of the new array to the stack
+    elements = std::move(new_elements);
+
+    // Update the capacity
+    capacity = new_capacity;
   }
 };
